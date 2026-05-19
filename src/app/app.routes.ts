560f86acdce25d5d4345ae879e7b2216d806fa71
Hotai-Lin <p230909@gosmio.biz>
2026-05-19 23:11:52 +0800

    隱藏管理員註冊路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { LoginComponent } from './pages/login/login';
-import { RegisterComponent } from './pages/register/register'; // 🌟 臨時註冊頁
+import { RegisterComponent } from './pages/register/register'; //  臨時註冊頁
 
 export const routes: Routes = [
   // 當網址是空的時候 (首頁)，載入 HomeComponent
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }, // 🌟 臨時註冊頁
+  // { path: 'register', component: RegisterComponent }, //  臨時註冊頁
   // 如果亂打網址，自動導回首頁
   { path: '**', redirectTo: '' } 
 ];
